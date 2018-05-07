78ec9158caf53ba302422e88e1ac7c37ba155875
Diane Xiaomeng Xie <tbaby.menger@gmail.com>
2018-05-07 16:31:07 -0400

    Upper the first character in the name

--- MySQLCommandLines.sql
+++ MySQLCommandLines.sql
@@ -2,3 +2,8 @@
 SELECT *
 FROM ToxicPlant AS t1 join ToxicPlant AS t2 on t1.ToxicPlantID = t2.ToxicPlantID
 WHERE t1.toxicPlantID = t2.toxicPlantId and t1.[Common Name] != t2.[Common Name];
+
+
+-- Upper the first character in the name :
+UPDATE ToxicPlant
+SET "Common Name" = UPPER(LEFT("Common Name",1))+LOWER(SUBSTRING("Common Name",2,LEN("Common Name")));
