adf58815510ed8ad7df7608c84e454c354599511
Diane Xiaomeng Xie <tbaby.menger@gmail.com>
2018-05-08 10:05:02 -0400

    Update MySQLCommandLines.sql

--- MySQLCommandLines.sql
+++ MySQLCommandLines.sql
@@ -7,3 +7,10 @@ WHERE t1.toxicPlantID = t2.toxicPlantId and t1.[Common Name] != t2.[Common Name]
 -- Upper the first character in the name :
 UPDATE ToxicPlant
 SET "Common Name" = UPPER(LEFT("Common Name",1))+LOWER(SUBSTRING("Common Name",2,LEN("Common Name")));
+
+
+-- Add several foreignkey at the same time:
+ALTER TABLE PlantLivestockSymptom
+ADD CONSTRAINT livestockSymptomFK
+FOREIGN KEY (ToxicPlantID) REFERENCES ToxicPlant (ToxicPlantID),
+FOREIGN KEY (LivestockSymptomID) REFERENCES [FFL_ToxicPlants].[dbo].[LivestockSymptom] (SymptomID);
