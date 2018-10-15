c2e840c55603669eee573ce7dcf04ef7419924b4
Diane Xiaomeng Xie <tbaby.menger@gmail.com>
2018-10-15 14:52:27 -0400

    Update CreateTriggerForTable.sql

--- CreateTriggerForTable.sql
+++ CreateTriggerForTable.sql
@@ -1,3 +1,11 @@
-CREATE TRIGGER test AFTER INSERT OR UPDATE OR DELETE ON t_trig_table
+-- For one table:
+CREATE TRIGGER test AFTER INSERT OR UPDATE OR DELETE ON t_trig_table 
 
         FOR EACH ROW EXECUTE PROCEDURE change_trigger();
+
+-- For multiple tables:
+CREATE TRIGGER testtrigger 
+AFTER INSERT OR UPDATE OR DELETE 
+ON 'table1 || table2 || table3 || ... '
+        FOR EACH ROW EXECUTE PROCEDURE change_trigger();
+      
